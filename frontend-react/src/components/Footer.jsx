873d7f5ce1a7882c548221a54d7989890bf93f85
Sushant Waghmare <sushantwaghmare206@gmail.com>
2025-08-20 16:02:28 +0530

    Homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+        <footer className='footer py-3 my-3'>
+            <hr className='border-bottom' />
+            <p className='text-light text-center'>&copy; 2025 - Built with Sushant</p>
+        </footer>
+    </>
+  )
+}
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from './Button'
+
+export const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Prediction Portal</h1>
+                <p className='text-light lead'>This stock prediction portal uses 100-day and 200 day moving averages</p>
+                <Button text = "Login" class = "btn-outline-warning"/>
+            </div>
+
+        </div>
+    </>
+  )
+}
